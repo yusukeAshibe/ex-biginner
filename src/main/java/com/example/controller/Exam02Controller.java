ece0caa0ff7ac40d9c31e5cba9892eb11ca90365
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jpgit config --listgit config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-02-04 15:21:41 +0900

    新規にコントローラーをさくせいしました

---
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -0,0 +1,30 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam02")
+public class Exam02Controller {
+
+	@Autowired
+	private HttpSession session;
+
+	@RequestMapping("")
+	public String index() {
+		return "exam02";
+	}
+
+	@RequestMapping("/sum")
+	public String sum(Integer num1, Integer num2) {
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
+		session.setAttribute("sum", num1 + num2);
+		
+		return "exam02";
+	}
+
+}
